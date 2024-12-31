a1c73f76884fbb346bf2ffef1b8cc1e13a95c1e5
araucaria <araucaria@lookfar.com>
2024-12-31 13:43:50 +0000

    Add default nixos module

---
+++ modules/nixos/default.nix
@@ -0,0 +1,64 @@
+{
+  config,
+  lib,
+  inputs,
+  outputs,
+  settings,
+  ...
+}: {
+  imports = lib.my.allNixFiles ./.;
+
+  # Static options
+  networking.hostName = "${settings.hostname}";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  services.openssh.enable = true;
+
+  # Firmware updates
+  services.fwupd.enable = true;
+  hardware.enableAllFirmware = true;
+
+  # Nix/Nixpkgs modifications
+  nixpkgs = {
+    overlays = with outputs.overlays; [
+      additions
+      modifications
+      unstable-packages
+    ];
+
+    # Allow unfree packages
+    config.allowUnfree = true;
+  };
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      experimental-features = [
+        # Enable flakes
+        "nix-command"
+        "flakes"
+        # Pipe operator |>
+        "pipe-operators"
+      ];
+
+      # Disable global registry
+      flake-registry = "";
+      # Optimise the nix store on rebuild
+      auto-optimise-store = true;
+    };
+
+    # Disable channels
+    channel.enable = false;
+
+    # Make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+
+    # Enable automatic garbage collection of generations older than 7 days
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 7d";
+    };
+  };
+}
