edfdacdca11c0acb7c59d9ea52a63bd7055a12d7
araucaria <araucaria@lookfar.com>
2025-01-06 12:06:59 +0000

    Add terminal directory to home-manager modules

---
+++ modules/home-manager/terminal/default.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = lib.my.allNixFiles ./.;
+
+  home.packages = with pkgs; [
+    # Disk usage analytics
+    ncdu
+  ];
+
+  # Youtube downloader
+  programs.yt-dlp.enable = true;
+}
+# Features enabled by default
+// lib.my.mapDefault true []
+# Features disabled by default
+// lib.my.mapDefault false []
