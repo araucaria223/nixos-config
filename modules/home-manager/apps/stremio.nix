3c5f829da9daaa9ccc18b51c9eb055d9282810fe
araucaria <araucaria@lookfar.com>
2025-01-07 22:08:33 +0000

    Add stremio module (enabled)

---
+++ modules/home-manager/apps/stremio.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options.stremio.enable = lib.my.mkDefaultTrueEnableOption "stremio";
+
+  config = lib.mkIf config.stremio.enable {
+    # Install stremio
+    home.packages = [pkgs.stremio];
+    # Persist stremio's data
+    home.persistence."/persist/home/${config.home.username}".directories = lib.my.symlink [
+      ".stremio-server"
+      ".local/share/Smart Code ltd/Stremio"
+      ".cache/Smart Code ltd/Stremio"
+    ];
+  };
+}
