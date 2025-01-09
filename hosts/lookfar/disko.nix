986caa180987c461f52dce6a838a8dc69fdf3b8a
araucaria223 <105236839+araucaria223@users.noreply.github.com>
2025-01-09 23:41:54 +0000

    Create disko.nix

---
+++ hosts/lookfar/disko.nix
@@ -0,0 +1,61 @@
+{device ? throw "Set this to your device, e.g. /dev/sda", ...}: {
+  disko.devices = {
+    disk.main = {
+      inherit device;
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = ["umask=0077"];
+            };
+          };
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "crypted";
+              passwordFile = "/tmp/secret.key";
+              settings.allowDiscards = true;
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"];
+                subvolumes = {
+                  "/persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = ["subvol=persist" "compress=zstd" "discard=async" "noatime"];
+                  };
+
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = ["subvol=nix" "compress=zstd" "discard=async" "noatime"];
+                  };
+
+                  "/swap" = {
+                    mountpoint = "/.swapvol";
+                    swap.swapfile.size = "8G";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+	"size=4G"
+	"defaults"
+	"mode=755"
+      ];
+    };
+  };
+}
