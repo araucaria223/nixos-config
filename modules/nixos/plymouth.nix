d37c04ec7e46962d4fb7fea06392e820e0f01c78
araucaria <araucaria@lookfar>
2025-01-10 13:09:40 +0000

    Add plymouth module

---
+++ modules/nixos/plymouth.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  options.plymouth.enable = lib.my.mkDefaultTrueEnableOption ''
+    plymouth boot splash screen
+  '';
+
+  config = lib.mkIf config.plymouth.enable {
+    stylix.targets.plymouth.logoAnimated = false;
+
+    boot = {
+      plymouth.enable = true;
+      consoleLogLevel = 0;
+      initrd.verbose = false;
+      kernelParams = [
+	"quiet"
+	"splash"
+	"boot.shell_on_fail"
+	"loglevel=3"
+	"rd.systemd.show_status=false"
+	"rd.udev.log_level=3"
+	"udev.log_priority=3"
+      ];
+
+      loader.timeout = 0;
+    };
+  };
+}
