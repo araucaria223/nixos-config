ba620a1f14e6b17879d4bef5cbbc7ff8887c062f
araucaria <araucaria@lookfar>
2025-01-10 14:16:13 +0000

    Formatting

--- modules/nixos/plymouth.nix
+++ modules/nixos/plymouth.nix
@@ -13,29 +13,30 @@
       plymouth.enable = true;
       consoleLogLevel = 0;
       initrd = {
-	verbose = false;
-	# Start systemd at stage 1 (for luks)
-	systemd.enable = true;
+        verbose = false;
+        # Start systemd at stage 1 (for luks)
+        systemd.enable = true;
       };
 
       # Silent boot options
       kernelParams = [
-	"quiet"
-	"splash"
-	"boot.shell_on_fail"
-	"loglevel=3"
-	"rd.systemd.show_status=false"
-	"rd.udev.log_level=3"
-	"udev.log_priority=3"
-	"vt.global_cursor_default=0"
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "loglevel=3"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+        "vt.global_cursor_default=0"
       ];
 
       loader.timeout = 0;
     };
-  };
 
-  services.getty = {
-    autologinUser = settings.username;
-    autologinOnce = true;
+    services.getty = {
+      autologinUser = settings.username;
+      autologinOnce = true;
+      loginProgram = "${config.programs.hyprland.package}/bin/Hyprland";
+    };
   };
 }
