a5f92c7d4644cfc3326c14c103d5cc4eb8f3c151
araucaria <araucaria@lookfar>
2025-01-20 22:52:03 +0000

    Add mullvad.nix

--- modules/nixos/mullvad.nix
+++ modules/nixos/mullvad.nix
@@ -8,11 +8,29 @@
     mullvad vpn
   '';
 
-  config.services = lib.mkIf config.mullvad.enable {
-    resolved.enable = true;
-    mullvad-vpn = {
-      enable = true;
-      package = pkgs.mullvad-vpn;
+  config = lib.mkIf config.mullvad.enable {
+    services = {
+      resolved.enable = true;
+      mullvad-vpn = {
+        enable = true;
+        package = pkgs.mullvad-vpn;
+      };
     };
+
+    sops.secrets.mullvad-device = {
+      format = "json";
+      sopsFile = lib.my.paths.secrets + /mullvad.json;
+      key = "";
+    };
+
+    environment.etc."mullvad-vpn/device.json".source = config.sops.secrets.mullvad-device.path;
+    systemd.services."mullvad-daemon".postStart = let
+      mullvad = lib.getExe' config.services.mullvad-vpn.package "mullvad";
+    in /*sh*/ ''
+      while ! ${mullvad} status >/dev/null; do sleep 1; done
+      ${mullvad} connect
+      ${mullvad} dns set default --block-ads --block-trackers --block-malware
+      ${mullvad} tunnel set wireguard --daita on
+    '';
   };
 }
