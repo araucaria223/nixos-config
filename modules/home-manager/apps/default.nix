e5ee0e2c28c620a79a31111551e4c9ce8b1982ad
araucaria <araucaria@lookfar>
2025-01-26 14:20:16 +0000

    Set xdg default applications

--- modules/home-manager/apps/default.nix
+++ modules/home-manager/apps/default.nix
@@ -10,5 +10,27 @@
     keepassxc
     # Torrent client
     qbittorrent
+    # GUI file manager
+    pcmanfm
   ];
+
+  # Set default applications
+  xdg.mimeApps = lib.mkDefault rec {
+    enable = true;
+    defaultApplications = let
+      browser = ["firefox.desktop"];
+    in {
+      "application/pdf" = ["zathura.desktop"];
+
+      "text/html" = browser;
+      "x-scheme-handler/http" = browser;
+      "x-scheme-handler/https" = browser;
+      "x-scheme-handler/about" = browser;
+      "x-scheme-handler/unknown" = browser;
+
+      "inode/directory" = ["pcmanfm.desktop"];
+    };
+
+    associations.added = defaultApplications;
+  };
 }
