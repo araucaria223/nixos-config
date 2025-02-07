69a8dcc9a4fedbbc8d797343acdbd6244fd658d9
araucaria <araucaria@lookfar>
2025-02-07 19:03:42 +0100

    Add hyprexpo keyboard binds

--- modules/home-manager/desktop/hyprland/plugins.nix
+++ modules/home-manager/desktop/hyprland/plugins.nix
@@ -10,16 +10,23 @@
       hyprexpo
     ];
 
-    settings.plugin = {
-      hyprexpo = {
-        columns = 3;
-        gap_size = 20;
-        workspace_method = "first 1";
-        enable_gesture = true;
-        gesture_fingers = 3;
-        gesture_distance = 200;
-        gesture_positive = true;
+    settings = {
+      plugin = {
+        hyprexpo = {
+          columns = 3;
+          gap_size = 20;
+          workspace_method = "first 1";
+          enable_gesture = true;
+          gesture_fingers = 3;
+          gesture_distance = 200;
+          gesture_positive = true;
+        };
       };
+
+      bind = [
+        "$mod, down, hyprexpo:expo, enable"
+        "$mod, up, hyprexpo:expo, disable"
+      ];
     };
   };
 }
