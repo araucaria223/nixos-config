09a3408ee2b895aebb8522fee1bba518987a7908
araucaria <araucaria@lookfar>
2025-03-09 11:36:05 +0000

    Revert swapping caps lock and escape

--- modules/home-manager/desktop/hypr/default.nix
+++ modules/home-manager/desktop/hypr/default.nix
@@ -30,7 +30,6 @@
         monitor = [",preffered,auto,1"];
         input = {
           follow_mouse = true;
-	  kb_options = "caps:swapescape";
           touchpad = {
             natural_scroll = false;
             scroll_factor = 0.6;
