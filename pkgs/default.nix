bc126c4343c1d99b8620cef6231a6f9dd24e79a1
araucaria <araucaria@lookfar>
2025-08-25 05:07:12 +0200

    Create shell app for ad-hoc network connections

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,4 +6,15 @@ pkgs: {
     runtimeInputs = with pkgs; [grim slurp swappy];
     text = ''pgrep slurp || grim -g "$(slurp)" - | swappy -f -'';
   };
+
+  connecto = pkgs.writeShellApplication {
+    name = "connecto";
+    runtimeInputs = with pkgs; [wpa_supplicant gnused];
+    text = ''
+      network = $(wpa_cli add_network | sed -n 2p)
+      wpa_cli set_network $network ssid "$1"
+      wpa_cli set_network $network psk "$2"
+      wpa_cli select_network $network
+    '';
+  };
 }
